c19f33475aedc86fb494b30782dafd4457f86e84
coreglitch <mail@s3f.ru>
2025-04-20 15:57:11 +0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  // Инициализация сериал-порта
+  Serial.begin(115200);
+  delay(1000);  // Небольшая задержка на запуск
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.setRotation(2);
+
+  // Сообщение
+  const char* msg = "Hello, Microrack!";
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  display.printf("Hello, Microrack!\n");
+
+  display.display();
+
+  // Вывод в консоль
+  Serial.println(msg);
+}
+
+void loop() {
+  // пусто
+}
