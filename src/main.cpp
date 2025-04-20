844d8a681e20de3efd552bea9282f7e277a32a1a
coreglitch <mail@s3f.ru>
2025-04-20 18:04:11 +0400

    power rails check

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 
 #include <Adafruit_MCP23X17.h>
 
+const int SCL_PIN = 22;
+const int SDA_PIN = 21;
+
+const int IO0_RST = 23;
+const int IO1_RST = 19;
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
@@ -16,8 +22,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 Adafruit_MCP23X17 mcp0;  // addr 0x20
 Adafruit_MCP23X17 mcp1;  // addr 0x21
 
-#define LED1_PIN 11  // GPB3 = pin 11 на MCP23017
-#define LED2_PIN 12  // GPB4 = pin 12 на MCP23017
+const int GPB = 8;
+const int LED1_PIN = GPB + 3;
+const int LED2_PIN = GPB + 4;
+
+const int P12V_PASS = GPB + 0;
+const int P5V_PASS = GPB + 1;
+const int M12V_PASS = GPB + 2;
 
 void setup() {
   // Инициализация сериал-порта
@@ -39,9 +50,23 @@ void setup() {
   display.printf("Hello, Microrack!\n");
   display.display();
 
+  pinMode(IO0_RST, OUTPUT);
+  digitalWrite(IO0_RST, LOW);   // удерживаем в сбросе
+  delay(10);
+  digitalWrite(IO0_RST, HIGH);  // выходим из сброса
+
+  pinMode(IO1_RST, OUTPUT);
+  digitalWrite(IO1_RST, LOW);   // удерживаем в сбросе
+  delay(10);
+  digitalWrite(IO1_RST, HIGH);  // выходим из сброса
+
   mcp1.begin_I2C(0x21);
   mcp1.pinMode(LED1_PIN, OUTPUT);
   mcp1.pinMode(LED2_PIN, OUTPUT);
+
+  mcp1.pinMode(P12V_PASS, INPUT);
+  mcp1.pinMode(P5V_PASS, INPUT);
+  mcp1.pinMode(M12V_PASS, INPUT);
 
   // Вывод в консоль
   Serial.println("Hello, Microrack!");
@@ -53,7 +78,13 @@ void loop() {
   mcp1.digitalWrite(LED1_PIN, state);
   mcp1.digitalWrite(LED2_PIN, !state);
 
-  Serial.println(state ? "LED1 ON, LED2 OFF" : "LED1 OFF, LED2 ON");
+  // Serial.println(state ? "LED1 ON, LED2 OFF" : "LED1 OFF, LED2 ON");
+
+  Serial.printf("+12: %d +5: %d -12: %d\n",
+    mcp1.digitalRead(P12V_PASS),
+    mcp1.digitalRead(P5V_PASS),
+    mcp1.digitalRead(M12V_PASS)
+  );
 
   state = !state;
   delay(500);
