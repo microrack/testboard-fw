2bee6282c7be1702962fe4715d84939a200efd9a
coreglitch <mail@s3f.ru>
2025-04-21 05:40:30 +0400

    style: reformat code to use 4 spaces for indentation

--- src/main.cpp
+++ src/main.cpp
@@ -43,99 +43,99 @@ DAC8552 dac1(PIN_CS1, &SPI_DAC);
 DAC8552 dac2(PIN_CS2, &SPI_DAC);
 
 void setup() {
-  // Инициализация сериал-порта
-  Serial.begin(115200);
-  delay(1000);  // Небольшая задержка на запуск
+    // Инициализация сериал-порта
+    Serial.begin(115200);
+    delay(1000);  // Небольшая задержка на запуск
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Don't proceed, loop forever
+    }
 
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-  display.setRotation(0);
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.printf("Hello, Microrack!\n");
-  display.display();
+    display.setTextSize(1);      // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.cp437(true);         // Use full 256 char 'Code Page 437' font
+    display.setRotation(0);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.printf("Hello, Microrack!\n");
+    display.display();
 
-  pinMode(IO0_RST, OUTPUT);
-  digitalWrite(IO0_RST, LOW);   // удерживаем в сбросе
-  delay(10);
-  digitalWrite(IO0_RST, HIGH);  // выходим из сброса
+    pinMode(IO0_RST, OUTPUT);
+    digitalWrite(IO0_RST, LOW);   // удерживаем в сбросе
+    delay(10);
+    digitalWrite(IO0_RST, HIGH);  // выходим из сброса
 
-  pinMode(IO1_RST, OUTPUT);
-  digitalWrite(IO1_RST, LOW);   // удерживаем в сбросе
-  delay(10);
-  digitalWrite(IO1_RST, HIGH);  // выходим из сброса
+    pinMode(IO1_RST, OUTPUT);
+    digitalWrite(IO1_RST, LOW);   // удерживаем в сбросе
+    delay(10);
+    digitalWrite(IO1_RST, HIGH);  // выходим из сброса
 
-  mcp1.begin_I2C(0x21);
-  mcp1.pinMode(LED1_PIN, OUTPUT);
-  mcp1.pinMode(LED2_PIN, OUTPUT);
+    mcp1.begin_I2C(0x21);
+    mcp1.pinMode(LED1_PIN, OUTPUT);
+    mcp1.pinMode(LED2_PIN, OUTPUT);
 
-  mcp1.pinMode(P12V_PASS, INPUT);
-  mcp1.pinMode(P5V_PASS, INPUT);
-  mcp1.pinMode(M12V_PASS, INPUT);
+    mcp1.pinMode(P12V_PASS, INPUT);
+    mcp1.pinMode(P5V_PASS, INPUT);
+    mcp1.pinMode(M12V_PASS, INPUT);
 
-  // Инициализация пинов CS
-  pinMode(PIN_CS1, OUTPUT);
-  pinMode(PIN_CS2, OUTPUT);
-  digitalWrite(PIN_CS1, HIGH);
-  digitalWrite(PIN_CS2, HIGH);
+    // Инициализация пинов CS
+    pinMode(PIN_CS1, OUTPUT);
+    pinMode(PIN_CS2, OUTPUT);
+    digitalWrite(PIN_CS1, HIGH);
+    digitalWrite(PIN_CS2, HIGH);
 
-  // Инициализация SPI
-  SPI_DAC.begin(PIN_SCK, -1, PIN_MOSI, -1); // SCK, MISO (нет), MOSI, SS (не используется)
-  dac1.begin();
-  dac2.begin();
+    // Инициализация SPI
+    SPI_DAC.begin(PIN_SCK, -1, PIN_MOSI, -1); // SCK, MISO (нет), MOSI, SS (не используется)
+    dac1.begin();
+    dac2.begin();
 
-  // Вывод в консоль
-  Serial.println("Hello, Microrack!");
+    // Вывод в консоль
+    Serial.println("Hello, Microrack!");
 }
 
 void writeDAC(int cs_pin, uint8_t channel, uint16_t value) {
-  uint8_t command = 0x00;
-  if (channel == 0) command = 0x00;         // DAC A
-  else if (channel == 1) command = 0x10;    // DAC B
-  else if (channel == 2) command = 0x20;    // both (A+B)
+    uint8_t command = 0x00;
+    if (channel == 0) command = 0x00;         // DAC A
+    else if (channel == 1) command = 0x10;    // DAC B
+    else if (channel == 2) command = 0x20;    // both (A+B)
 
-  uint8_t highByte = (value >> 8) & 0xFF;
-  uint8_t lowByte  = value & 0xFF;
+    uint8_t highByte = (value >> 8) & 0xFF;
+    uint8_t lowByte  = value & 0xFF;
 
-  digitalWrite(cs_pin, LOW);
-  SPI_DAC.transfer(command);
-  SPI_DAC.transfer(highByte);
-  SPI_DAC.transfer(lowByte);
-  digitalWrite(cs_pin, HIGH);
-  delayMicroseconds(100);
+    digitalWrite(cs_pin, LOW);
+    SPI_DAC.transfer(command);
+    SPI_DAC.transfer(highByte);
+    SPI_DAC.transfer(lowByte);
+    digitalWrite(cs_pin, HIGH);
+    delayMicroseconds(100);
 }
 
 void loop() {
-  static bool state = false;
+    static bool state = false;
 
-  mcp1.digitalWrite(LED1_PIN, state);
-  mcp1.digitalWrite(LED2_PIN, !state);
+    mcp1.digitalWrite(LED1_PIN, state);
+    mcp1.digitalWrite(LED2_PIN, !state);
 
-  // Serial.println(state ? "LED1 ON, LED2 OFF" : "LED1 OFF, LED2 ON");
+    // Serial.println(state ? "LED1 ON, LED2 OFF" : "LED1 OFF, LED2 ON");
 
-  Serial.printf("+12: %d +5: %d -12: %d\n",
-    mcp1.digitalRead(P12V_PASS),
-    mcp1.digitalRead(P5V_PASS),
-    !mcp1.digitalRead(M12V_PASS)
-  );
+    Serial.printf("+12: %d +5: %d -12: %d\n",
+        mcp1.digitalRead(P12V_PASS),
+        mcp1.digitalRead(P5V_PASS),
+        !mcp1.digitalRead(M12V_PASS)
+    );
 
-  static uint16_t value = 0;
-  dac1.setValue(0, value);
-  dac1.setValue(1, value);
-  dac2.setValue(0, value);
-  dac2.setValue(1, value);
-  value += 5000;
-  if(value > 65536 - 5000) {
-    value = 0;
-  }
+    static uint16_t value = 0;
+    dac1.setValue(0, value);
+    dac1.setValue(1, value);
+    dac2.setValue(0, value);
+    dac2.setValue(1, value);
+    value += 5000;
+    if(value > 65536 - 5000) {
+        value = 0;
+    }
 
-  state = !state;
-  delay(500);
+    state = !state;
+    delay(500);
 }
