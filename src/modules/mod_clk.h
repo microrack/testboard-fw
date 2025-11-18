bc448a4771e184a6c6fb8c6fe9aca97cef3cb4bf
glitchcore <mail@s3f.ru>
2025-11-19 00:35:20 +0400

    mod-clk

--- src/modules/mod_clk.h
+++ src/modules/mod_clk.h
@@ -13,11 +13,27 @@
 // alias out_8=11
 
 void mod_clk_init() {
-    
+    hal_set_io(IO1, IO_INPUT);
+    hal_set_io(IO2, IO_INPUT);
+    hal_set_io(IO3, IO_INPUT);
+
 }
 
+const int TIME = 10;
+
 void mod_clk_handler() {
+    for(size_t i = 0; i < 8; i++) {
+        hal_set_io(IO0, IO_HIGH); delay(60); hal_set_io(IO0, IO_LOW);
+    }
+    for(size_t i = 0; i < 8; i++) {
+        hal_set_io(IO0, IO_HIGH); delayMicroseconds(TIME); hal_set_io(IO0, IO_LOW);
+        hal_set_io(IO1, IO_HIGH); delayMicroseconds(TIME); hal_set_io(IO1, IO_INPUT);
+    }
     
+    for(size_t i = 0; i < 8; i++) {
+        hal_set_io(IO0, IO_HIGH); delayMicroseconds(TIME); hal_set_io(IO0, IO_LOW);
+        hal_set_io(IO3, IO_HIGH); delayMicroseconds(TIME); hal_set_io(IO3, IO_INPUT);
+    }
 }
 
 #define MOD_CLK_ID 1
