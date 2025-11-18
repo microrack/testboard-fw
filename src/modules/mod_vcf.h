24105933e0caccfe2aedefbe74302e56ff2832b9
glitchcore <mail@s3f.ru>
2025-11-18 23:54:56 +0400

    add vcf

--- src/modules/mod_vcf.h
+++ src/modules/mod_vcf.h
@@ -4,8 +4,8 @@
 // alias cutoff_pot=in zE
 // alias depth_pot=in zD
 // alias hp_mode=1
-// alias input_1=in pdA
-// alias input_2=in pdB
+// alias input_1=out A
+// alias input_2=out B
 // alias lp_mode=0
 // alias output_ab_1=in A
 // alias output_ab_2=in B
@@ -16,11 +16,15 @@
 // alias signal_lp=out A
 
 void mod_vcf_init() {
-    
+    hal_set_source(SOURCE_D, 0);
+    hal_set_io(IO2, IO_LOW);
+    hal_start_signal(SOURCE_A, 140.0f, WAVEFORM_SAWTOOTH);
+    hal_start_signal(SOURCE_B, 140.0f * 3/4, WAVEFORM_SAWTOOTH);
 }
 
 void mod_vcf_handler() {
-    
+    hal_set_source(SOURCE_D, 5000); delay(100); hal_set_source(SOURCE_D, 0); delay(100);
+    hal_set_io(IO2, IO_HIGH); delay(100); hal_set_io(IO2, IO_LOW); delay(100);
 }
 
 #define MOD_VCF_ID 16
